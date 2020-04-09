9f753ecef8ea3a6015a32fd433257e6e5377d8e4
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-04-09 16:05:35 -0400

    adding cleaning step

--- mx-health-ministry/pipeline.py
+++ mx-health-ministry/pipeline.py
@@ -1,5 +1,10 @@
+from io import StringIO
+import numpy as np
+import re
 import argparse
 from pathlib import Path
+
+import pandas as pd
 
 from ploomber import DAG, SourceLoader
 from ploomber.products import File, GenericProduct
@@ -25,6 +30,60 @@ confirmed = ShellScript(loader['get_confirmed.sh'],
 suspected = ShellScript(loader['get_suspected.sh'],
                         File('data/suspected.csv'), dag)
 
+
+def parse_bad_line(line, regex):
+    elements = re.findall(regex, line)
+    return ','.join(elements[0])
+
+
+def _clean(upstream, product, regex):
+    lines = np.array(Path(str(upstream.first)).read_text().split('\n'))
+
+    idxs = np.zeros(len(lines)).astype(int)
+
+    regex_good = re.compile(r'^\d+,')
+    regex_first_page = re.compile(r'^\d+')
+
+    for i, line in enumerate(lines):
+        if re.match(regex_good, line):
+            idxs[i] = 1
+        elif re.match(regex_first_page, line):
+            idxs[i] = 2
+
+    lines_good = lines[idxs == 1]
+    lines_bad = lines[idxs == 2]
+    lines_fixed = [parse_bad_line(l, regex) for l in lines_bad]
+
+    fixed = lines_fixed + list(lines_good)
+    content = '\n'.join(fixed)
+
+    names = ['N Caso', 'Estado', 'Sexo', 'Edad',
+             'Fecha de inicio de síntomas',
+             'Identificación de COVID-19 por RT-PCR en tiempo real']
+    df = pd.read_csv(StringIO(content), names=names)
+    df.to_csv(str(product), index=False)
+
+
+confirmed_regex = re.compile(r'^(\d+)\s{1}([\w\s]+)\s{1}(FEMENINO|MASCULINO)\s{1}(\d+),(.+),(Confirmado)')
+suspected_regex = re.compile(r'^(\d+)\s{1}([\w\s]+)\s{1}(FEMENINO|MASCULINO)\s{1}(\d+)\s{1}(.+)\s{1}(Sospechoso)')
+
+clean_confirmed = PythonCallable(_clean,
+                                 File(ROOT / 'confirmed_clean.csv'),
+                                 dag,
+                                 name='clean_confirmed',
+                                 params={'regex': confirmed_regex})
+
+clean_suspected = PythonCallable(_clean,
+                                 File(ROOT / 'suspected_clean.csv'),
+                                 dag,
+                                 name='clean_suspected',
+                                 params={'regex': suspected_regex})
+
+
+confirmed >> clean_confirmed
+suspected >> clean_suspected
+
+
 if args.upload:
     upload_confirmed = UploadToS3('{{upstream["get_confirmed.sh"]}}',
                                   GenericProduct('mx_confirmed_s3'), dag,
@@ -35,8 +94,10 @@ if args.upload:
                                   bucket='mx-covid-data',
                                   name='upload_mx_suspected')
 
-    confirmed >> upload_confirmed
+    clean_confirmed >> upload_confirmed
     suspected >> upload_suspected
 
 
-dag.build()
+table = dag.build()
+
+print(table)
